2f11ca4a25b9b17a7c14f4cb524bbe6b78c71e4d
ika9 <nuraziqah92@gmail.com>
2021-10-04 12:48:35 +0800

    Update index.php
    Sehingga operator

--- index.php
+++ index.php
@@ -21,11 +21,39 @@
 
     $namapelajar="Ali";
     echo "<h2>Hello $namapelajar</h2>";
-
-    $hadir= false;
+    $namapelajar="Hidayati";
     $umur= 20;
+    echo "<h2>Hello $namapelajar umur $umur</h2>";
+
     $Nokp='0112233445566';
 
+    $x = 10;
+    $y='12';
+
+    if ($x != $y) {
+        echo 'Tak sama';
+    } else {
+        echo 'masih sama';
+    }
+
      ?>
+     <p>
+         <?php echo 20 . 21 ?>
+
+     </p>
+
+     <p>
+         <?php
+         $bandar = 'Pasir gudang';
+         $baru= 'bandar: ';
+
+         for($x = 0; $x < strlen($bandar); $x++) {
+             $baru .= $bandar[$x] . '';
+         }
+
+         echo $baru;
+         ?>
+
+     </p>
 </body>
 </html>
