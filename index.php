65335422aa881a020e1ca6b0d92878e050fee223
ika9 <nuraziqah92@gmail.com>
2021-10-04 12:02:57 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1>Asas php</h1>
+     
+
+    <?php 
+    //komen satu baris
+    #komen satu baris
+    /*
+
+    komen byk baris
+    
+    */
+
+    $namapelajar="Ali";
+    echo "<h2>Hello $namapelajar</h2>";
+
+    $hadir= false;
+    $umur= 20;
+    $Nokp='0112233445566';
+
+     ?>
+</body>
+</html>
